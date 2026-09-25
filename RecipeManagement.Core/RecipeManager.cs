07a0354d1a58c4b68d4a09c75195dac3529dd967
ductien531 <ductien.van@student.adelaide.edu.au>
2026-09-25 18:33:38 +0930

    Add comments for constructor and methods

--- RecipeManagement.Core/RecipeManager.cs
+++ RecipeManagement.Core/RecipeManager.cs
@@ -18,10 +18,14 @@ public sealed class RecipeManager : IRecipeManager
     private Stack<int> _removedRecipes = new Stack<int>();
 
     private Queue<string> _instructionsQueue = new Queue<string>();
+
+    /// <summary>
+    /// This method will build the recipe catalogue from the supplied recipes
+    /// </summary>
     public RecipeManager(IEnumerable<Recipe> recipes)
     {
         // TODO Part A: validate recipes and build Dictionary<int, Recipe>.
-        if (recipes is null)
+        if (recipes is null) // If supplied recipes is null, it will throw ArgumentNullException
         {
             throw new ArgumentNullException("Recipes can not be null");
         }
@@ -33,7 +37,7 @@ public sealed class RecipeManager : IRecipeManager
                 throw new ArgumentNullException("Recipe entry must not be null");
             }
 
-            if (recipe.Id <= 0)
+            if (recipe.Id <= 0) // Recipe ID can not be negative or equal 0
             {
                 throw new ArgumentException("Recipe's ID must be positive");
             }
@@ -43,7 +47,7 @@ public sealed class RecipeManager : IRecipeManager
                 throw new ArgumentException("Recipe's title must not be null");
             }
 
-            if (_recipes.ContainsKey(recipe.Id))
+            if (_recipes.ContainsKey(recipe.Id)) // Dictionary can not have duplicated keys
             {
                 throw new ArgumentException("Recipe's must not be duplicated");
             }
@@ -57,6 +61,10 @@ public sealed class RecipeManager : IRecipeManager
     public int PendingInstructionCount => _instructionsQueue.Count;
     public int RemovedRecipeCount => _removedRecipes.Count;
 
+    /// <summary>
+    /// This method will add supplied recipe into the dictionary (_recipes). Besides that, invalid value will be rejected and 
+    /// return False
+    /// </summary>
     public bool AddRecipe(Recipe recipe)
     {
         if (recipe is null)
@@ -69,7 +77,7 @@ public sealed class RecipeManager : IRecipeManager
             return false;
         }
 
-        _recipes.Add(recipe.Id, recipe);
+        _recipes.Add(recipe.Id, recipe); // This valid value will be added to the dictionary
         return true;
     }
 
@@ -85,6 +93,10 @@ public sealed class RecipeManager : IRecipeManager
         }
     }
 
+    /// <summary>
+    /// This method will remove a recipe from the dictionary. If the recipe Id exists in the _cookingPlan, it can not be 
+    /// deleted and return false
+    /// </summary>
     public bool RemoveRecipe(int recipeId)
     {
         if (_cookingPlan.Contains(recipeId))
@@ -98,6 +110,10 @@ public sealed class RecipeManager : IRecipeManager
         }
     }
 
+    /// <summary>
+    /// This method will copy a recipe from the original _recipes and add recipe's ingredient into _shoppingList. It will 
+    /// return 0 if the recipe is unfounded
+    /// </summary>
     public int AddIngredientsToShoppingList(int recipeId)
     {
         if (!_recipes.TryGetValue(recipeId, out Recipe? recipe))
@@ -134,6 +150,9 @@ public sealed class RecipeManager : IRecipeManager
         }
     }
 
+    /// <summary>
+    /// This method will remove a recipe from _cookingPlan and push it to _removedRecipes as the most recently removed
+    /// </summary>
     public bool RemoveRecipeFromCookingPlan(int recipeId)
     {
         bool flag = _cookingPlan.Remove(recipeId);
@@ -145,9 +164,12 @@ public sealed class RecipeManager : IRecipeManager
         return true;
     }
 
+    /// <summary>
+    /// This method will return the most recently removed recipe and place it to the last index of the cooking plan
+    /// </summary>
     public bool RestoreLastRemovedRecipe()
     {
-        if(!_removedRecipes.TryPop(out int recipeID))
+        if(!_removedRecipes.TryPop(out int recipeID)) // It will return false if the recipe Id doesn't exist in _removedRecipes
         {
             return false;
         }
@@ -182,6 +204,9 @@ public sealed class RecipeManager : IRecipeManager
         return new List<int>(_cookingPlan);
     }
 
+    /// <summary>
+    /// For an existing recipe with at least one instruction, clear the current queue, enqueue the instruction strings in their original order and return true. Otherwise return false
+    /// </summary>
     public bool StartCooking(int recipeId)
     {
         if(!_recipes.TryGetValue(recipeId, out Recipe? recipe) || recipe.Instructions.Count == 0)
@@ -200,7 +225,7 @@ public sealed class RecipeManager : IRecipeManager
         }
     }
     /// <summary>
-    /// 
+    /// This method will peek the nex instruction from the _instructionsQueue. If the _instructionQueue doesn't have any instruction, it will return null
     /// </summary>
     /// <returns></returns>
     public string PeekNextInstruction()
@@ -216,9 +241,8 @@ public sealed class RecipeManager : IRecipeManager
     }
 
     /// <summary>
-    /// 
+    /// This method will  removes and returns exactly one item from the front
     /// </summary>
-    /// <returns></returns>
     public string CompleteNextInstruction()
     {
         if(_instructionsQueue.TryDequeue(out string? instruction))
